6a0184557329b47072f4ceca93e1777de021ea73
Ryzhij_Kotik <hbychkova@gmail.com>
2021-11-24 03:58:16 +0300

    refactoring

--- tests/tests/test_put_dictionary.py
+++ tests/tests/test_put_dictionary.py
@@ -10,7 +10,8 @@ from copy import deepcopy
 def test_put_dictionary():
     new_dict = deepcopy(RequestData.valid_dict)
     new_dict["value"] = f"new {RequestData.valid_dict['value']}"
-    response = requests.put(URL.host + URL.path_dictionary_key, json=new_dict)
+    response = requests.put(URL.host + URL.path_dictionary_key.format(key=new_dict["key"]),
+                            json=new_dict)
 
     assert response.status_code == ResponseStatus.OK
     response_body = response.json()
@@ -20,7 +21,7 @@ def test_put_dictionary():
 
 def test_put_not_existing_item(key_not_existing):
     body = RequestData.generate_item(key_not_existing)
-    response = requests.put(URL.host + URL.path_dictionary_key,
+    response = requests.put(URL.host + URL.path_dictionary_key.format(key=key_not_existing),
                              json=body)
 
     assert response.status_code == ResponseStatus.NOT_FOUND
@@ -29,7 +30,7 @@ def test_put_not_existing_item(key_not_existing):
 
 @pytest.mark.parametrize("invalid_body", InvalidRequestData.invalid_dicts)
 def test_put_invalid_body(invalid_body):
-    response = requests.put(URL.host + URL.path_dictionary_key,
+    response = requests.put(URL.host + URL.path_dictionary_key.format(key="test"),
                              json=invalid_body)
 
     assert response.status_code == ResponseStatus.BAD_REQUEST
